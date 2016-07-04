30cb1da03d90cdfd81b5070217922486b6730242
acsauk <acsauk@gmail.com>
2016-07-04 17:22:33 +0100

    Code removed from soltuon #1, starting solution #2

--- lib/fizzbuzz_kata.rb
+++ lib/fizzbuzz_kata.rb
@@ -1,11 +1,3 @@
-def fizz_buzz(input)
-  if input.to_i % 3 == 0 && input.to_i % 5 == 0
-    "fizzbuzz"
-  elsif input.to_i % 3 == 0
-    "fizz"
-  elsif input.to_i % 5 == 0
-    "buzz"
-  else
-    "#{input}"
-  end
+def method
+  
 end
