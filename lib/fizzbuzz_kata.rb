2e00d6861600883e76dd273af8c6128572ceb3b7
acsauk <acsauk@gmail.com>
2016-07-07 18:22:45 +0100

    delete attempt #2 code

--- lib/fizzbuzz_kata.rb
+++ lib/fizzbuzz_kata.rb
@@ -1,19 +1,3 @@
 def fizz_buzz(num)
-  if div_3(num.to_i) && div_5(num.to_i)
-    "fizzbuzz"
-  elsif div_3(num.to_i)
-    "fizz"
-  elsif div_5(num.to_i)
-    "buzz"
-  else
-    "#{num}"
-  end
-end
 
-def div_3(num)
-  num % 3 == 0
-end
-
-def div_5(num)
-  num % 5 == 0
 end
