40005b1449bb04afe02be56715cb2b5ef9d8b58a
Alex Saunders <alex@existent.co.uk>
2017-01-25 12:04:11 +0000

    refactors spec to use contexts for when a number is and is not divisible by a number

--- spec/fizzbuzz_kata_spec.rb
+++ spec/fizzbuzz_kata_spec.rb
@@ -1,15 +1,19 @@
 require 'fizzbuzz_kata'
 
 describe 'FizzBuzz' do
-  it 'knows when a number is divisible by 3' do
-    expect(is_divisible_by_three(3)).to eq true
+  context 'knows when a number is divisible by' do
+    it 3 do
+      expect(is_divisible_by_three(3)).to eq true
+    end
+
+    it 5 do
+      expect(is_divisible_by_five(5)).to eq true
+    end
   end
 
-  it 'knows when a number is not divisble by 3' do
-    expect(is_divisible_by_three(2)).to eq false
-  end
-
-  it 'knows when a number is divisible by 5' do
-    expect(is_divisible_by_five(5)).to eq true
+  context 'knows when a number is not divisible by' do
+    it 3 do
+      expect(is_divisible_by_three(2)).to eq false
+    end
   end
 end
