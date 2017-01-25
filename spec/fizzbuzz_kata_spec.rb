dc0e0b3670f4fc5494f6558d8273e9e4d673a16f
Alex Saunders <alex@existent.co.uk>
2017-01-25 11:57:18 +0000

    creates failing test for is_divisible_by_five

--- spec/fizzbuzz_kata_spec.rb
+++ spec/fizzbuzz_kata_spec.rb
@@ -8,4 +8,8 @@ describe 'FizzBuzz' do
   it 'knows when a number is not divisble by 3' do
     expect(is_divisible_by_three(2)).to eq false
   end
+
+  it 'knows when a number is divisible by 5' do
+    expect(is_divisible_by_five(5)).to eq true
+  end
 end
