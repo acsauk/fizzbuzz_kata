018b58134da311d6366ca8214f4c2a8db428fbef
Alex Saunders <alex@existent.co.uk>
2017-01-25 11:44:27 +0000

    fixes typo in method name in spec

--- spec/fizzbuzz_kata_spec.rb
+++ spec/fizzbuzz_kata_spec.rb
@@ -2,6 +2,6 @@ require 'fizzbuzz_kata'
 
 describe 'FizzBuzz' do
   it 'knows when a number is divisible by 3' do
-    expect(is_diisible_by_three(3)).to eq true
+    expect(is_divisible_by_three(3)).to eq true
   end
 end
